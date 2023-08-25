b7c700fc0a0152401d1347ad63d37e8cd95e21f8
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-08-25 23:08:13 +0300

    base configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  mode: 'universal',
+  target: 'server',
+  head: {
+    title: 'HortDesign',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0,' },
+      { hid: 'Portfolio', name: 'Portfolio', content: 'Portfolio' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+  },
+  css: [
+    '~/src/styles/index.scss'
+  ],
 })
